055af90c07dd5d6e81a2dfd9597f8de60b701124
petr-prikryl <petrik.prikryl@gmail.com>
2023-10-09 20:14:50 +0200

    final

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from unidecode import unidecode
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from ui_generated import Ui_MainWindow
 
 
 def gcd(a, b):
@@ -81,3 +84,40 @@ def decrypt(text, a, b):
     result = result.replace('XMEZERAX', ' ')  # vracecka mezer
 
     return result
+
+
+class AffineCipherApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Initialize the UI
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Connect UI elements to functions
+        self.ui.pushButton_encrypt.clicked.connect(self.encrypt_text)
+        self.ui.pushButton_decrypt.clicked.connect(self.decrypt_text)
+
+    def encrypt_text(self):
+        a = int(self.ui.lineEdit_a.text())
+        b = int(self.ui.lineEdit_b.text())
+        text = self.ui.plainTextEdit_input.toPlainText()
+
+        filtered_text = filter_input(text)
+        encrypted_text = encrypt(filtered_text, a, b)
+
+        self.ui.plainTextEdit_output.setPlainText(encrypted_text)
+
+    def decrypt_text(self):
+        a = int(self.ui.lineEdit_a.text())
+        b = int(self.ui.lineEdit_b.text())
+        text = self.ui.plainTextEdit_input.toPlainText()
+        decrypted_text = decrypt(text, a, b)
+        self.ui.plainTextEdit_output.setPlainText(decrypted_text)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = AffineCipherApp()
+    window.show()
+    sys.exit(app.exec_())
